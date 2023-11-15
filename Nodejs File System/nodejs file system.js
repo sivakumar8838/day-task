2edcbc15ad904ac21b0c2bab6c632564d55ced4e
siva <sivamss5882@gmail.com>
2023-11-15 09:47:37 +0530

    hi

--- Nodejs File System/nodejs file system.js
+++ Nodejs File System/nodejs file system.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server =http.createServer((req, res) => {
     res.statusCode =200;
     res.setHeader('Content-type', 'text/plain');
-    res.end('hello world\n');
+    res.end('hello\n');
 });
 
 server.listen(port, hostname, () => {
