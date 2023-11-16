8975bfb7beb07f480f5b8a8c3f37f6f9c523bb2f
siva <sivamss5882@gmail.com>
2023-11-16 20:14:19 +0530

    #

--- server.js
+++ server.js
@@ -1,117 +1,254 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const { MONGODB_URI, PORT } = require('./uils/config');
 
 const app = express();
-const port = 3000;
 
-app.use(bodyParser.json());
+app.use(express.json());
 
-// Local variables to store data
-const rooms = [
-  { id: 1, seats: 10, amenities: ['Projector', 'Whiteboard'], pricePerHour: 50 },
-  { id: 2, seats: 8, amenities: ['TV', 'Coffee Machine'], pricePerHour: 40 },
-  { id: 3, seats: 12, amenities: ['Projector', 'Conference Phone'], pricePerHour: 60 },
-  { id: 4, seats: 6, amenities: ['Whiteboard'], pricePerHour: 30 },
-  { id: 5, seats: 15, amenities: ['Projector', 'Coffee Machine'], pricePerHour: 70 },
-  { id: 6, seats: 20, amenities: ['TV', 'Whiteboard'], pricePerHour: 80 },
-  { id: 7, seats: 8, amenities: ['Coffee Machine'], pricePerHour: 35 },
-  { id: 8, seats: 10, amenities: ['TV', 'Whiteboard', 'Conference Phone'], pricePerHour: 65 },
-  { id: 9, seats: 12, amenities: ['Projector'], pricePerHour: 55 },
-  { id: 10, seats: 6, amenities: ['Coffee Machine', 'Whiteboard'], pricePerHour: 40 },
-  { id: 11, seats: 15, amenities: ['TV', 'Conference Phone'], pricePerHour: 75 },
-  { id: 12, seats: 8, amenities: ['Projector'], pricePerHour: 45 },
-  { id: 13, seats: 10, amenities: ['Coffee Machine', 'Whiteboard'], pricePerHour: 55 },
-  { id: 14, seats: 12, amenities: ['TV', 'Conference Phone'], pricePerHour: 70 },
-];
+app.use(cors());
 
-const bookings = [
-  { id: 1, customerName: 'John Doe', date: '2023-11-15', startTime: '10:00 AM', endTime: '12:00 PM', roomId: 1 },
-  { id: 2, customerName: 'Jane Smith', date: '2023-11-16', startTime: '02:00 PM', endTime: '04:00 PM', roomId: 2 },
-  { id: 3, customerName: 'Bob Johnson', date: '2023-11-17', startTime: '01:00 PM', endTime: '03:00 PM', roomId: 3 },
-  { id: 4, customerName: 'Alice Brown', date: '2023-11-18', startTime: '03:00 PM', endTime: '05:00 PM', roomId: 4 },
-  { id: 5, customerName: 'Charlie Davis', date: '2023-11-19', startTime: '11:00 AM', endTime: '01:00 PM', roomId: 5 },
-  { id: 6, customerName: 'Eva White', date: '2023-11-20', startTime: '09:00 AM', endTime: '11:00 AM', roomId: 6 },
-  { id: 7, customerName: 'David Lee', date: '2023-11-21', startTime: '12:00 PM', endTime: '02:00 PM', roomId: 7 },
-  { id: 8, customerName: 'Grace Miller', date: '2023-11-22', startTime: '10:00 AM', endTime: '12:00 PM', roomId: 8 },
-  { id: 9, customerName: 'Samuel Turner', date: '2023-11-23', startTime: '02:00 PM', endTime: '04:00 PM', roomId: 9 },
-  { id: 10, customerName: 'Sophia Moore', date: '2023-11-24', startTime: '11:00 AM', endTime: '01:00 PM', roomId: 10 },
-  { id: 11, customerName: 'Liam Harris', date: '2023-11-25', startTime: '03:00 PM', endTime: '05:00 PM', roomId: 11 },
-  { id: 12, customerName: 'Olivia Clark', date: '2023-11-26', startTime: '09:00 AM', endTime: '11:00 AM', roomId: 12 },
-  { id: 13, customerName: 'Noah Allen', date: '2023-11-27', startTime: '01:00 PM', endTime: '03:00 PM', roomId: 13 },
-  { id: 14, customerName: 'Ava Hall', date: '2023-11-28', startTime: '12:00 PM', endTime: '02:00 PM', roomId: 14 },
-];
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    console.log('Connecting to MongoDB...');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 
-// Welcome message for the root path
+const mentorSchema = new mongoose.Schema({
+  mentor_id: Number,
+  mentor_name: String,
+  course: String,
+  mentees: Array
+}, { versionKey: false });
+
+const studentSchema = new mongoose.Schema({
+  student_id: Number,
+  student_name: String,
+  mentor_id: Number,
+  course: String,
+}, { versionKey: false }); 
+
+const studentsAll = mongoose.model('studentsAll', studentSchema, 'stduents'); 
+const mentors = mongoose.model('mentors', mentorSchema, 'mentors');
+
 app.get('/', (req, res) => {
-  res.send('Welcome to the room booking system!');
+  res.send(`   <h1 style="color: #333; text-align: center;">All End Points Display:</h1>
+
+  <div style="margin: 20px;">
+      <table style="width: 100%; border-collapse: collapse; margin-top: 20px;">
+          <tr style="background-color: #f2f2f2;">
+              <th style="border: 1px solid #ddd; padding: 8px; text-align: left;">Endpoint</th>
+              <th style="border: 1px solid #ddd; padding: 8px; text-align: left;">Description</th>
+          </tr>
+          <tr>
+              <td style="border: 1px solid #ddd; padding: 8px;">/student</td>
+              <td style="border: 1px solid #ddd; padding: 8px;">Student Information</td>
+          </tr>
+          <tr>
+              <td style="border: 1px solid #ddd; padding: 8px;">/mentors</td>
+              <td style="border: 1px solid #ddd; padding: 8px;">Mentors Information</td>
+          </tr>
+          <tr>
+              <td style="border: 1px solid #ddd; padding: 8px;">/create/student</td>
+              <td style="border: 1px solid #ddd; padding: 8px;">Endpoint to create a new student</td>
+          </tr>
+          <tr>
+              <td style="border: 1px solid #ddd; padding: 8px;">/create/mentor</td>
+              <td style="border: 1px solid #ddd; padding: 8px;">Endpoint to create a new mentor</td>
+          </tr>
+          <tr>
+              <td style="border: 1px solid #ddd; padding: 8px;">/assignmentor/:mentorId/:studentId</td>
+              <td style="border: 1px solid #ddd; padding: 8px;">Endpoint to assign a student to a mentor</td>
+          </tr>
+          <tr>
+              <td style="border: 1px solid #ddd; padding: 8px;">/nomentor</td>
+              <td style="border: 1px solid #ddd; padding: 8px;">Endpoint to get students without a mentor</td>
+          </tr>
+          <tr>
+              <td style="border: 1px solid #ddd; padding: 8px;">/assign-mentor/:studentId/:mentorId</td>
+              <td style="border: 1px solid #ddd; padding: 8px;">Endpoint to assign or change mentor for a particular student</td>
+          </tr>
+          <tr>
+              <td style="border: 1px solid #ddd; padding: 8px;">/particularmentor/:mentorId</td>
+              <td style="border: 1px solid #ddd; padding: 8px;">Endpoint to show all students for a particular mentor</td>
+          </tr>
+          <tr>
+              <td style="border: 1px solid #ddd; padding: 8px;">/previous-mentor/:studentId</td>
+              <td style="border: 1px solid #ddd; padding: 8px;">Endpoint to show the previously assigned mentor for a particular student</td>
+          </tr>
+      </table>
+  </div>`)
+})
+
+app.get('/student', (req, res) => {
+  studentsAll.find({}, {})
+    .then(students => {
+      res.json(students);
+    })
+    .catch(error => {
+      console.error('Error fetching students:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
 });
 
-// Create a Room
-app.post('/createRoom', (req, res) => {
-  const { seats, amenities, pricePerHour } = req.body;
-  const room = {
-    id: rooms.length + 1,
-    seats,
-    amenities,
-    pricePerHour,
-  };
-  rooms.push(room);
-  res.json(room);
+app.get('/mentors', (req, res) => {
+  mentors.find({}, {})
+    .then(mentor => {
+      res.json(mentor);
+    })
+    .catch(error => {
+      console.error('Error fetching students:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
 });
 
-// Book a Room
-app.post('/bookRoom', (req, res) => {
-  const { customerName, date, startTime, endTime, roomId } = req.body;
-  const room = rooms.find((r) => r.id === roomId);
-  if (!room) {
-    return res.status(404).json({ error: 'Room not found' });
+// Endpoint to create a new student
+app.post('/create/student', (request, response) => {
+  const student = new studentsAll(request.body);
+  student.save()
+    .then(() => {
+      response.status(201).json({ message: 'Student created successfully' });
+    })
+    .catch(error => {
+      console.error('Error creating student:', error);
+      response.status(500).json({ error: 'Internal Server Error' });
+    });
+});
+
+// Endpoint to create a new mentor
+app.post('/create/mentor', (request, response) => {
+  const mentor = new mentors(request.body);
+  mentor.save()
+    .then(() => {
+      response.status(201).json({ message: 'Mentor created successfully' });
+    })
+    .catch(error => {
+      console.error('Error creating mentor:', error);
+      response.status(500).json({ error: 'Internal Server Error' });
+    });
+});
+// Endpoint to assign a student to a mentor
+app.post('/assignmentor/:mentorId/:studentId', async (req, res) => {
+  try {
+    const mentorId = req.params.mentorId;
+    const studentId = req.params.studentId;
+
+    // Check if the mentor and student exist
+    const mentor = await mentors.findOne({ mentor_id: mentorId });
+    const student = await studentsAll.findOne({ student_id: studentId });
+
+    if (!mentor || !student) {
+      return res.status(404).json({ error: 'Mentor or student not found' });
+    }
+
+    // Check if the student already has a mentor
+    if (student.mentor_id) {
+      return res.status(400).json({ error: 'Student already has a mentor' });
+    }
+
+    // Assign the mentor to the student
+    student.mentor_id = mentorId;
+    await student.save();
+
+    // Add the student to the mentor's mentees list
+    mentor.mentees.push({ student_id: studentId });
+    await mentor.save();
+
+    res.status(200).json({ message: 'Student assigned to mentor successfully' });
+  } catch (error) {
+    console.error('Error assigning mentor to student:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
-  const booking = {
-    id: bookings.length + 1,
-    customerName,
-    date,
-    startTime,
-    endTime,
-    roomId,
-  };
-  bookings.push(booking);
-  res.json(booking);
 });
 
-// List all Rooms with Booked Data
-app.get('/listRooms', (req, res) => {
-  const roomList = rooms.map((room) => {
-    const bookedData = bookings.filter((booking) => booking.roomId === room.id);
-    return {
-      roomName: `Room ${room.id}`,
-      bookedData,
-    };
-  });
-  res.json(roomList);
+// Endpoint to get students without a mentor
+app.get('/nomentor', async (req, res) => {
+  try {
+    const studentsWithoutMentor = await studentsAll.find({ mentor_id: null });
+    res.json(studentsWithoutMentor);
+  } catch (error) {
+    console.error('Error fetching students without mentor:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+// Endpoint to assign or change mentor for a particular student
+app.post('/assign-mentor/:studentId/:mentorId', async (req, res) => {
+  try {
+    const studentId = req.params.studentId;
+    const newMentorId = req.params.mentorId;
+
+    // Check if the student exists
+    const student = await studentsAll.findOne({ student_id: studentId });
+
+    if (!student) {
+      return res.status(404).json({ error: 'Student not found' });
+    }
+
+    // Check if the new mentor exists
+    const newMentor = await mentors.findOne({ mentor_id: newMentorId });
+
+    if (!newMentor) {
+      return res.status(404).json({ error: 'Mentor not found' });
+    }
+
+    // Change the mentor for the student
+    student.mentor_id = newMentorId;
+    await student.save();
+
+    res.status(200).json({ message: 'Mentor assigned or changed successfully' });
+  } catch (error) {
+    console.error('Error assigning or changing mentor:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
-// List all customers with booked Data
-app.get('/listCustomers', (req, res) => {
-  const customerList = bookings.map((booking) => {
-    const room = rooms.find((r) => r.id === booking.roomId);
-    return {
-      customerName: booking.customerName,
-      roomName:` Room ${room.id}`,
-      date: booking.date,
-      startTime: booking.startTime,
-      endTime: booking.endTime,
-    };
-  });
-  res.json(customerList);
+// Endpoint to show all students for a particular mentor
+app.get('/paticularmentor/:mentorId', async (req, res) => {
+  try {
+    const mentorId = req.params.mentorId;
+
+    // Check if the mentor exists
+    const mentor = await mentors.findOne({ mentor_id: mentorId });
+
+    if (!mentor) {
+      return res.status(404).json({ error: 'Mentor not found' });
+    }
+
+    // Get all students for the mentor
+    const studentsForMentor = await studentsAll.find({ mentor_id: mentorId });
+    res.json(studentsForMentor);
+  } catch (error) {
+    console.error('Error fetching students for mentor:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
-// List how many times a customer has booked the room
-app.get('/customerBookingHistory/:customerName', (req, res) => {
-  const { customerName } = req.params;
-  const customerHistory = bookings.filter((booking) => booking.customerName === customerName);
-  res.json(customerHistory);
+// Endpoint to show the previously assigned mentor for a particular student
+app.get('/previous-mentor/:studentId', async (req, res) => {
+  try {
+    const studentId = req.params.studentId;
+
+    // Check if the student exists
+    const student = await studentsAll.findOne({ student_id: studentId });
+
+    if (!student) {
+      return res.status(404).json({ error: 'Student not found' });
+    }
+
+    // Get the previously assigned mentor
+    const previousMentorId = student.mentor_id;
+    const previousMentor = await mentors.findOne({ mentor_id: previousMentorId });
+
+    res.json(previousMentor);
+  } catch (error) {
+    console.error('Error fetching previous mentor:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
